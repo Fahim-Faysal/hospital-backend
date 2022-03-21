308039d4849a30c9d21ce5840066c5d0ab1b0c78
Fahim-Faysal <faysalsiyam@gmail.com>
2022-03-21 20:05:55 +0600

    add

--- index.js
+++ index.js
@@ -21,15 +21,58 @@ async function run() {
             await client.connect()
             const database = client.db("hospital");
             const userCollection = database.collection("users")
+            const medicaltest = database.collection('medicalTest')
+            const services = database.collection('services')
+            const appointmentCollection = database.collection('appointments')
+            const bookedAppointments = database.collection('bookedappointments')
             // const messeageCollection = database.collection("messeages");
             // const reviewCollection = database.collection("reviews");
             // const bookingCollection = database.collection("booking")
             // const serviceCollection = database.collection('services')
             // const designCollection = database.collection('design')
             // const todaysOrderCollectin = database.collection('todays_design_order')
-            console.log("connected");
-            const user = { name: 'Fahim Faysal siyam', email: 'faysalsiyam@gmail.com' }
-            userCollection.insertOne(user)
+            app.get('/medical', async (req, res) => {
+                  const cursor = medicaltest.find({});
+                  const result = await cursor.toArray()
+                  res.send(result)
+            })
+            app.post('/addservice', async (req, res) => {
+                  const data = req.body
+                  const result = await services.insertOne(data)
+                  res.json(result)
+            })
+            app.get('/appointments', async (req, res) => {
+                  const cursor = appointmentCollection.find({})
+                  const result = await cursor.toArray()
+                  res.send(result)
+            })
+            app.post('/appointment', async (req, res) => {
+                  const data = req.body
+                  const result = await bookedAppointments.insertOne(data)
+                  res.json(result)
+            })
+            app.get('/appointments/:email', async (req, res) => {
+                  const email = req.params.email;
+                  const query = { email: email };
+                  const user = await appointmentCollection.findOne(query)
+                  let isAdmin = false;
+                  if (user?.role === 'admin') {
+                        isAdmin = true;
+                  }
+                  res.json({ admin: isAdmin })
+            })
+            app.delete('/appointments/:id', async (req, res) => {
+                  const id = req.params.id;
+                  console.log(id);
+                  const query = { _id: ObjectId(id) }
+                  const result = await bookedAppointments.findOne(query)
+                  res.json(result)
+            })
+            // app.post('/user', async (req, res) => {
+            //       const user = req.body;
+            //       const result = await userCollection.insertOne(user)
+            //       res.json(result)
+            // })
       }
       catch {
             // await client.close();
